4a9ea9a151525d53795fe65750c3e367c61e573b
Kolibrika666 <kolibrika666@gmail.com>
2024-09-27 16:56:30 +0700

    init: calculator-body

---
+++ src/Calculator/ButtonBody.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ButtonBody = () => {
+    return (
+        <div className='calculator__Body'>
+            тело
+        </div>
+    );
+};
+
+export default ButtonBody;
