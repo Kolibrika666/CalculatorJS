2d84ea20294aceb152770549e0c3d6d180587cbc
Kolibrika666 <kolibrika666@gmail.com>
2024-09-27 16:56:16 +0700

    init: calculator-body

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import ButtonBody from './Calculator/ButtonBody'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+<ButtonBody/>
     </>
   )
 }
